71411bb12e496e24cdd44ec1883333414c6b96bb
AsgharB <abhutta@Asghar.local>
2020-09-27 17:51:03 -0400

    addded feature of high scores clear and highscores clearing in the DOM

--- js/scores.js
+++ js/scores.js
@@ -1,14 +1,28 @@
 function displayScores(){
-
     var highScoresEl = document.getElementById("highscores");
     var data = JSON.parse(window.localStorage.getItem("highscores"));
     if(data){
-        highScoresEl.innerHTML = data;
+        highScoresEl.innerHTML = "<p>Initials: " + data[0]['initials'] + "</p><p>Score: " + data[0]['score'] + "</p>";
     }else{
         console.log("didn't work");
     }
-    
-    console.log(data)
+}
+
+
+function clearScores(){
+
+    localStorage.clear();
+
+    initials = "";
+    score = 0;
+
+    var highScoresEl = document.getElementById("highscores");
+    if(highScoresEl){
+        highScoresEl.innerHTML = "<p>Initials: " + initials + "</p><p>Score: " + score + "</p>";
+    }else{
+        console.log("didn't work");
+    }
+
 }
 
 displayScores();
