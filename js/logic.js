c205f39164c1ef04090feb38aa1eae814dbc5862
AsgharB <abhutta@Asghar.local>
2020-09-27 19:28:17 -0400

    CSS styling for buttons added and small alignment issues

--- js/logic.js
+++ js/logic.js
@@ -103,7 +103,7 @@ function questionClick() {
     feedbackEl.setAttribute("class","feedback");
     setTimeout(function(){
         feedbackEl.setAttribute("class","feedback hide");
-    },1000)
+    },1000);
     
     //next question
     currentQuestionIndex++
